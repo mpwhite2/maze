a07cfdc33c9a3b606d7cad404a2aeba9353cf317
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2023-03-14 18:20:21 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,42 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    info.changeScoreBy(20)
     game.over(true)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.toggleViewMode()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.jumpWithHeightAndDuration(Player1, 48, 1000)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    if (Render.isSpritesOverlapZ(sprite, otherSprite)) {
+        otherSprite.destroy(effects.confetti, 500)
+        info.changeLifeBy(1)
+    }
 })
 info.onLifeZero(function () {
     game.over(false)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeScoreBy(1)
-    Points += -1
+    if (Render.isSpritesOverlapZ(sprite, otherSprite)) {
+        otherSprite.destroy()
+        info.changeScoreBy(1)
+    }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    otherSprite.destroy()
+    if (Render.isSpritesOverlapZ(sprite, otherSprite)) {
+        info.changeLifeBy(-1)
+        otherSprite.destroy(effects.blizzard, 500)
+        sprite.startEffect(effects.ashes, 500)
+        music.wawawawaa.play()
+        scene.cameraShake(8, 500)
+        pause(1000)
+    }
 })
 let Snack: Sprite = null
+let Chest: Sprite = null
 let Enemy1: Sprite = null
+let Player1: Sprite = null
 scene.setBackgroundImage(img`
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
@@ -138,29 +160,12 @@ scene.setBackgroundImage(img`
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
-let Player1 = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(Player1)
+Player1 = Render.getRenderSpriteInstance()
 scene.cameraFollowSprite(Player1)
 Player1.x += 6
-info.setLife(2)
-for (let index = 0; index < 4; index++) {
+info.setLife(5)
+info.setScore(0)
+for (let index = 0; index < 30; index++) {
     Enemy1 = sprites.create(img`
         ........................
         ........................
@@ -187,11 +192,31 @@ for (let index = 0; index < 4; index++) {
         ........................
         ........................
         `, SpriteKind.Enemy)
-    Enemy1.follow(Player1, 80)
-    tiles.placeOnRandomTile(Enemy1, assets.tile`myTile2`)
+    tiles.placeOnRandomTile(Enemy1, assets.tile`transparency16`)
+    Enemy1.follow(Player1, 50)
 }
-let Points = 20
-for (let index = 0; index < 20; index++) {
+for (let index = 0; index < 40; index++) {
+    Chest = sprites.create(img`
+        . . b b b b b b b b b b b b . . 
+        . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+        b e e e e e e e e e e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b b b b b b b d d b b b b b b b 
+        c b b b b b b c c b b b b b b c 
+        c c c c c c b c c b c c c c c c 
+        b e e e e e c b b c e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b c e e e e e e e e e e e e c b 
+        b b b b b b b b b b b b b b b b 
+        . b b . . . . . . . . . . b b . 
+        `, SpriteKind.Projectile)
+    tiles.placeOnRandomTile(Chest, assets.tile`transparency16`)
+}
+for (let index = 0; index < 100; index++) {
     Snack = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -212,11 +237,41 @@ for (let index = 0; index < 20; index++) {
         `, SpriteKind.Food)
     tiles.placeOnRandomTile(Snack, assets.tile`transparency16`)
 }
+let Enemy2 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .....fffc1111111f.......
+    ...fc111cd1111111f......
+    ...f1b1b1b1111dddf......
+    ...fbfbffcf11fcddf......
+    ......fcf111111bbf......
+    .......ccbdb1b1fcf......
+    .......fffbfbfdff.......
+    ........ffffffff........
+    ........fffffffffff.....
+    .........fffffc111cf....
+    .........fffff1b1b1f....
+    ..........ffffbfbfbf....
+    ...........ffff.........
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+Enemy2.setFlag(SpriteFlag.GhostThroughWalls, false)
+tiles.placeOnRandomTile(Enemy2, assets.tile`transparency16`)
+Enemy2.follow(Player1, 80)
+Render.moveWithController(3)
 forever(function () {
-    music.playMelody("E B C5 A B G A F ", 120)
-    if (Points == 0) {
-        tiles.setWallAt(tiles.getTileLocation(1, 5), false)
-        tiles.setTileAt(tiles.getTileLocation(1, 5), assets.tile`transparency16`)
-        tiles.setTileAt(tiles.getTileLocation(1, 5), assets.tile`myTile0`)
+    music.playMelody("C5 A B G A F G E ", 120)
+    if (info.score() > 19) {
+        tiles.setWallAt(tiles.getTileLocation(1, 1), false)
+        tiles.setTileAt(tiles.getTileLocation(1, 1), assets.tile`transparency16`)
+        tiles.setTileAt(tiles.getTileLocation(1, 1), assets.tile`myTile0`)
     }
 })
