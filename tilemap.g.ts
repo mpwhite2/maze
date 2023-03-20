70f52a20fb4a2de6278e704bc3c86d692343f650
mpwhite2 <110784371+mpwhite2@users.noreply.github.com>
2023-03-19 19:23:45 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,6 +8,8 @@ namespace myTiles {
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -173,6 +175,8 @@ namespace myTiles {
             case "tile3":return tile3;
             case "myTile2":
             case "tile2":return tile2;
+            case "myTile1":
+            case "tile4":return tile4;
         }
         return null;
     })
